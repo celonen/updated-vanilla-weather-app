ed492cfd42f46ac45fcaa9ef1dd2346051fddcf9
celonen <84405152+celonen@users.noreply.github.com>
2021-06-03 13:11:49 +0200

    cleaning code

--- src/app.js
+++ src/app.js
@@ -69,7 +69,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let key = "5f472b7acba333cd8a035ea85a0d4d4c";
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
   axios.get(url).then(displayForecast);
